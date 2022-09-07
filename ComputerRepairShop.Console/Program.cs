fd0d707d87ff56123e3493dcef989306a1567093
Geniels <genielsantos010@gmail.com>
2022-09-07 18:27:14 -0300

    Update program.cs

--- ComputerRepairShop.Console/Program.cs
+++ ComputerRepairShop.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ComputerRepairShop.Domain.Modules;
+using System;
 
 namespace ComputerRepairShop.Console
 {
@@ -6,7 +7,39 @@ namespace ComputerRepairShop.Console
     {
         static void Main(string[] args)
         {
-            
+           Address address = new Address();
+           Client client = new Client();
+
+            System.Console.WriteLine("************************************************************");
+            System.Console.WriteLine("****************************MENU****************************");
+            System.Console.WriteLine("Selec one option with numbers");
+            System.Console.WriteLine("Register new client = 1");
+            System.Console.WriteLine("Register product = 2");
+            var optionMenu = int.Parse(System.Console.ReadLine());
+
+            if ( optionMenu == 1)
+            {  
+                System.Console.WriteLine("Selected option 'Register new client' ");
+                System.Console.WriteLine();
+                System.Console.Write("What you name?: ");
+                client.Name = System.Console.ReadLine();
+                System.Console.Write("What you state?: ");
+                address.State = System.Console.ReadLine();
+                System.Console.Write("What you city?: ");
+                address.City = System.Console.ReadLine();
+                System.Console.Write("What you CEP?: ");
+                address.Cep = System.Console.ReadLine();
+                System.Console.Write("What you street?: ");
+                address.Street = System.Console.ReadLine();
+                System.Console.Write("What you street number?: ");
+                address.StreetNumber = int.Parse(System.Console.ReadLine());
+            }
+            else if ( optionMenu == 2)
+            {
+                System.Console.WriteLine("Selected option 'Register product' ");
+                System.Console.WriteLine();
+                System.Console.WriteLine();
+            }
         }
     }
 }
