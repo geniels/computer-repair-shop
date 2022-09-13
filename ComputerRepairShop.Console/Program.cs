ebf13bebfd6b56578c9bb50d0a02755ba6da5444
Geniels <genielsantos010@gmail.com>
2022-09-12 23:56:32 -0300

    Updated Console.

--- ComputerRepairShop.Console/Program.cs
+++ ComputerRepairShop.Console/Program.cs
@@ -1,5 +1,7 @@
-﻿using ComputerRepairShop.Domain.Modules;
+﻿using ComputerRepairShop.Domain.Enum;
+using ComputerRepairShop.Domain.Modules;
 using System;
+using System.Xml.Schema;
 
 namespace ComputerRepairShop.Console
 {
@@ -7,8 +9,9 @@ namespace ComputerRepairShop.Console
     {
         static void Main(string[] args)
         {
-           Address address = new Address();
-           Client client = new Client();
+            Address address = new Address();
+            Client client = new Client();
+            Computer computer = new Computer();
 
             System.Console.WriteLine("************************************************************");
             System.Console.WriteLine("****************************MENU****************************");
@@ -17,8 +20,8 @@ namespace ComputerRepairShop.Console
             System.Console.WriteLine("Register product = 2");
             var optionMenu = int.Parse(System.Console.ReadLine());
 
-            if ( optionMenu == 1)
-            {  
+            if (optionMenu == 1)
+            {
                 System.Console.WriteLine("Selected option 'Register new client' ");
                 System.Console.WriteLine();
                 System.Console.Write("What you name?: ");
@@ -34,12 +37,28 @@ namespace ComputerRepairShop.Console
                 System.Console.Write("What you street number?: ");
                 address.StreetNumber = int.Parse(System.Console.ReadLine());
             }
-            else if ( optionMenu == 2)
+            else if (optionMenu == 2)
             {
                 System.Console.WriteLine("Selected option 'Register product' ");
                 System.Console.WriteLine();
-                System.Console.WriteLine();
+                System.Console.WriteLine("what is the product id ");
+                computer.Id = int.Parse(System.Console.ReadLine());
+                System.Console.WriteLine("Portable? S/N");
+                var idBool = System.Console.ReadLine();
+                if (idBool == "s" && idBool == "S")
+                {
+                    computer.Portable = true;
+                }
+                else if (idBool == "n" && idBool == "n"){ 
+                    computer.Portable = false;
+                }
+                else { System.Console.WriteLine("S OR N ONLY"); }
+                System.Console.WriteLine("What the service?");       
+                
+                System.Console.WriteLine(Enum.GetName(typeof(ServiceTypes), 0));
+
             }
+
         }
     }
 }
